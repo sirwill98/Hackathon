7c1c9a3c8497011297ae9e1277a982c032b5b819
Danny <danny.chan@uni.strath.ac.uk>
2019-04-02 16:31:50 +0100

    added style for donate step 1 and 2

--- RegisterDonate.js
+++ RegisterDonate.js
@@ -1,6 +1,22 @@
+function ContinueToStep2()
+{
+    //Continue to step 2
+}
+
+function ContinueToStep3()
+{
+    //Continue to step 3
+}
+
+function removeElement(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+}
+
 function DonateAgeYes()
 {
-    document.getElementById("Age").style.visibility = "hidden";
+    removeElement("Age");
     document.getElementById("AgeSuccess").style.visibility = "visible";
     document.getElementById("LocationLabel").style.visibility = "visible";
     document.getElementById("Location").style.visibility = "visible";
@@ -14,7 +30,7 @@ function DonateAgeNo()
 
 function DonateLocationYes()
 {
-    document.getElementById("Location").style.visibility = "hidden";
+    removeElement("Location");
     document.getElementById("LocationSuccess").style.visibility = "visible";
     document.getElementById("AttributesLabel").style.visibility = "visible";
     document.getElementById("Attributes").style.visibility = "visible";
@@ -30,7 +46,45 @@ function SubmitAttributes()
 {
 
 }
-function DonateRegister()
-{
 
+function HealthOK()
+{
+    removeElement("Health");
+    document.getElementById("Healthy").style.visibility = "visible";
+    document.getElementById("DrugsLabel").style.visibility = "visible";
+    document.getElementById("Drugs").style.visibility = "visible";
+}
+
+function HealthNotOK()
+{
+    document.getElementById("Health").style.visibility = "hidden";
+    document.getElementById("NotHealthy").style.visibility = "visible";
+
+}
+
+function DrugsYes()
+{
+    document.getElementById("Drugs").style.visibility = "hidden";
+    document.getElementById("TakenDrugs").style.visibility = "visible";
+}
+
+function DrugsNo()
+{
+    removeElement("Drugs");
+    document.getElementById("NoDrugs").style.visibility = "visible";
+    document.getElementById("MedicalConditionsLabel").style.visibility = "visible";
+    document.getElementById("MedicalConditions").style.visibility = "visible";
+}
+
+function MedicalConditionsYes()
+{
+    document.getElementById("ConditionsNotOK").style.visibility = "hidden";
+    document.getElementById("TakenDrugs").style.visibility = "visible";
+}
+
+function MedicalConditionsNo()
+{
+    removeElement("MedicalConditions");
+    document.getElementById("ConditionsOK").style.visibility = "visible";
+    document.getElementById("ContinueToStep3").style.visibility = "visible";
 }
