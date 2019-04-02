8136e07536d3cff2d9ea12f6289f8b64dae09f15
billy <billyboy2410@hotmail.co.uk>
2019-04-02 12:37:11 +0100

    Donate commit FUK

---
+++ RegisterDonate.js
@@ -0,0 +1,16 @@
+function DonateAge()
+{
+    document.getElementById("location").style.visibility = "visible";
+}
+function DonateLocation()
+{
+    document.getElementById("attributes").style.visibility = "visible";
+}
+function SubmitAttributes()
+{
+
+}
+function DonateRegister()
+{
+
+}
