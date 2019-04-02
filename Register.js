ede0348e69a0e79f28e100350ab29c1621b8ce28
billy <billyboy2410@hotmail.co.uk>
2019-04-02 13:28:17 +0100

    Began implimentation of user handling code

---
+++ Register.js
@@ -0,0 +1,12 @@
+function register()
+{
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var passwordConf = document.getElementById("passwordConf");
+
+    if(password === passwordConf)
+    {
+
+    }
+}
