6735424e75e5ff384c8d49c4d7a1340de2eee212
billy <billyboy2410@hotmail.co.uk>
2019-04-02 15:20:43 +0100

    users working and have uid

--- Register.js
+++ Register.js
@@ -4,11 +4,15 @@ function RegisterUser()
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var passwordConf = document.getElementById("passwordConf").value;
+    var d = new Date();
+    var uid = d.getTime();
+    uid = uid/name.length;
+    uid = Math.round(uid);
     console.log("test");
 
     if(password === passwordConf)
     {
-        var user = {name:name, email:email, password:password};
+        var user = {name:name, email:email, password:password, uid:uid};
         RegisterJSON(user);
     }
 }
