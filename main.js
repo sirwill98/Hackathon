84946737f2d699b2668d865cd3d6b9450d79e3b1
fmb16144 <thejester129@gmail.com>
2019-04-03 14:38:47 +0100

    add main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict;"
+//collapsible menu
+function init() {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content =document.getElementById("dropdown");
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+}
